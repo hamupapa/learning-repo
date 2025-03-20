be57905923fd17038f7bae62f2a1ab9de36852d0
hamupapa <k.mine2012@gmail.com>
2025-03-20 23:24:30 +0900

    update

--- control/src/hello.cpp
+++ control/src/hello.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <cstring>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "hello.h"
+
+void send_message() {
+
+}
+
+void receive_message() {
+
+}
+
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
